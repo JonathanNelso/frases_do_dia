060880eaed897da285e4059efa8f797370c9dc80
JonathanNelso <jonathanneves33@gmail.com>
2020-05-02 12:29:53 -0300

    iniciando o projeto

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(MaterialApp(
+    title: "Frases do dia",
+    home:  Container(
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          Text("teste"),
+        ],
+      ),
+    ),
+  ));
+
+}
